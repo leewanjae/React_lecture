a56b266a0b800b0556b3e532edfde18b42b50f43
leewanjae <dhkswo1@gmail.com>
2023-05-07 00:22:01 +0900

    Add UseState & AddEvent

--- components/PostList.js
+++ components/PostList.js
@@ -1,12 +1,31 @@
+import { useState } from "react";
+import NewPost from "./NewPost";
 import Post from "./Post";
 import styles from "./PostList.moduel.css";
 
 function PostList(props) {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
-    <ul className={styles.posts}>
-      <Post author="name" body="leewanjae" />
-      <Post author="age" body="28" />
-    </ul>
+    <>
+      <NewPost
+        onBodyChange={bodyChangeHandler}
+        onAuthorChange={authorChangeHandler}
+      />
+      <ul className={styles.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="age" body="28" />
+      </ul>
+    </>
   );
 }
 
