9c3b51fa5b42e7f719cbc209169e60f9fdf8f327
leewanjae <dhkswo1@gmail.com>
2023-05-07 00:56:33 +0900

    Add Modal

---
+++ components/Modal.js
@@ -0,0 +1,14 @@
+import styles from "./Modal.module.css";
+
+function Modal({ children }) {
+  return (
+    <>
+      <div className={styles.backdrop} />
+      <dialog open className={styles.modal}>
+        {children}
+      </dialog>
+    </>
+  );
+}
+
+export default Modal;
